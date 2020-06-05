acf7ea63add74f9d5034b8b6d8dfe79063d205aa
andrija-mitrovic <57568556+andrija-mitrovic@users.noreply.github.com>
2020-06-05 18:22:04 +0200

    Initial Commit

---
+++ PaymentComputeApp.WebUI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PaymentComputeApp.WebUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
