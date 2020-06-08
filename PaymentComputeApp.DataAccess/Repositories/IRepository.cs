d96b32a7d9e27a684ebeb77d7e37b92fca4ec095
andrija-mitrovic <57568556+andrija-mitrovic@users.noreply.github.com>
2020-06-08 12:48:09 +0200

    Generic Repository Added

---
+++ PaymentComputeApp.DataAccess/Repositories/IRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaymentComputeApp.DataAccess.Repositories
+{
+    public interface IRepository<TEntity> where TEntity:class
+    {
+        Task<IEnumerable<TEntity>> GetAsync(
+           Expression<Func<TEntity, bool>> filter = null,
+           Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+           string includeProperties = "");
+        Task<TEntity> GetByIdAsync(int id);
+        TEntity GetById(int id);
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate);
+        Task AddAsync(TEntity entity);
+        Task AddRangeAsync(IEnumerable<TEntity> entities);
+        void Remove(int id);
+        void Remove(TEntity entity);
+        void RemoveRange(IEnumerable<TEntity> entities);
+        void Update(TEntity entityToUpdate);
+    }
+}
---
+++ PaymentComputeApp.DataAccess/Repositories/Repository.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaymentComputeApp.DataAccess.Repositories
+{
+    public class Repository<TEntity>:IRepository<TEntity> where TEntity:class
+    {
+        protected readonly ApplicationDbContext _context;
+        private DbSet<TEntity> _dbSet;
+
+        public Repository(ApplicationDbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<TEntity>();
+        }
+
+        public virtual async Task<IEnumerable<TEntity>> GetAsync(
+           Expression<Func<TEntity, bool>> filter = null,
+           Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+           string includeProperties = "")
+        {
+            IQueryable<TEntity> query = _dbSet;
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            foreach (var includeProperty in includeProperties.Split
+                (new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+            {
+                query = query.Include(includeProperty);
+            }
+
+            return (orderBy != null) ? await orderBy(query).ToListAsync() : await query.ToListAsync();
+        }
+
+        public async Task<TEntity> GetByIdAsync(int id)
+            => await _context.Set<TEntity>().FindAsync(id);
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync()
+            => await _context.Set<TEntity>().ToListAsync();
+
+        public IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate)
+            => _context.Set<TEntity>().Where(predicate);
+
+        public async Task AddAsync(TEntity entity)
+            => await _context.Set<TEntity>().AddAsync(entity);
+
+        public async Task AddRangeAsync(IEnumerable<TEntity> entities)
+            => await _context.Set<TEntity>().AddRangeAsync(entities);
+
+        public void Remove(TEntity entity)
+            => _context.Set<TEntity>().Remove(entity);
+
+        public void RemoveRange(IEnumerable<TEntity> entities)
+            => _context.Set<TEntity>().RemoveRange(entities);
+
+        public void Update(TEntity entityToUpdate)
+            => _context.Set<TEntity>().Update(entityToUpdate);
+
+        public void Remove(int id)
+            => _context.Set<TEntity>().Remove(_context.Set<TEntity>().Find(id));
+
+        public TEntity GetById(int id)
+            => _context.Set<TEntity>().Find(id);
+    } 
+}
