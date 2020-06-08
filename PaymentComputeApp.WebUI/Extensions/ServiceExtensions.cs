eb5f1b8217c12ddc09f0fbbde17d7630b4f242cb
andrija-mitrovic <57568556+andrija-mitrovic@users.noreply.github.com>
2020-06-08 12:29:53 +0200

    ServiceExtensions Added

--- PaymentComputeApp.WebUI/Extensions/ServiceExtensions.cs
+++ PaymentComputeApp.WebUI/Extensions/ServiceExtensions.cs
@@ -1,4 +1,13 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PaymentComputeApp.DataAccess;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,6 +16,38 @@ namespace PaymentComputeApp.WebUI.Extensions
 {
     public static class ServiceExtensions
     {
+        public static void ConfigureCookiePolicyOptions(this IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+        }
 
+        public static void ConfigureMVC(this IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        public static void ConfigureIdentityBuilder(this IServiceCollection services)
+        {
+            services.AddDefaultIdentity<IdentityUser>()
+                .AddDefaultUI(UIFramework.Bootstrap4)
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+        }
+
+        public static void ConfigureSqlServer(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    config.GetConnectionString("DefaultConnection")));
+        }
+
+        public static void ConfigureService(this IServiceCollection services)
+        {
+
+        }
     }
 }
--- PaymentComputeApp.WebUI/Startup.cs
+++ PaymentComputeApp.WebUI/Startup.cs
@@ -10,9 +10,9 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using PaymentComputeApp.WebUI.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using PaymentComputeApp.WebUI.Extensions;
 
 namespace PaymentComputeApp.WebUI
 {
@@ -28,21 +28,11 @@ namespace PaymentComputeApp.WebUI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
-
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>()
-                .AddDefaultUI(UIFramework.Bootstrap4)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
-
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.ConfigureCookiePolicyOptions();
+            services.ConfigureSqlServer(Configuration);
+            services.ConfigureIdentityBuilder();
+            services.ConfigureMVC();
+            services.ConfigureService();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
