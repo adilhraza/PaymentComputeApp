be2b8102bc348de52556babd1f8525bc43b60912
andrija-mitrovic <57568556+andrija-mitrovic@users.noreply.github.com>
2020-06-19 22:50:37 +0200

    ReportController PaymentByDate Action Added

---
+++ PaymentComputeApp.WebUI/Controllers/ReportController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PaymentComputeApp.DataAccess.Repositories;
+using PaymentComputeApp.WebUI.Models;
+
+namespace PaymentComputeApp.WebUI.Controllers
+{
+    public class ReportController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ReportController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<IActionResult> PaymentByDate()
+        { 
+            PaymentByDateReportViewModel model = new PaymentByDateReportViewModel()
+            {
+                DateFrom = DateTime.Now,
+                DateTo = DateTime.Now,
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PaymentByDate(PaymentByDateReportViewModel paymentReportModel)
+        {
+                var paymentsRepo = (await _unitOfWork.PaymentRepository.GetAsync(includeProperties: "Employee",
+                    filter: x=>x.PayDate>=paymentReportModel.DateFrom && x.PayDate<=paymentReportModel.DateTo))
+                    .Select(payment => new PaymentIndexViewModel()
+                    {
+                        Id = payment.Id,
+                        EmployeeId = payment.EmployeeId,
+                        FullName = payment.Employee.FirstName + " " + payment.Employee.LastName,
+                        PayDate = payment.PayDate,
+                        PayMonth = payment.PayMonth,
+                        TaxYearId = payment.TaxYearId,
+                        Year = _unitOfWork.TaxYearRepository.GetById(payment.TaxYearId).YearOfTax,
+                        TotalEarnings = payment.TotalEarnings,
+                        TotalDeduction = payment.TotalDeduction,
+                        NetPayment = payment.NetPayment,
+                        Employee = payment.Employee
+                    });
+
+            PaymentByDateReportViewModel model = new PaymentByDateReportViewModel()
+                {
+                    DateFrom = DateTime.Now,
+                    DateTo = DateTime.Now,
+                    Payments = paymentsRepo
+                };
+
+                return View(model);
+        }
+    }
+}
