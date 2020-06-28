7274cbd8e410c734090953e097d484b888bdbd4a
andrija-mitrovic <57568556+andrija-mitrovic@users.noreply.github.com>
2020-06-28 10:52:04 +0200

    TaxController Index Action Added

---
+++ PaymentComputeApp.WebUI/Controllers/TaxController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PaymentComputeApp.DataAccess.Repositories;
+using PaymentComputeApp.WebUI.Models;
+
+namespace PaymentComputeApp.WebUI.Controllers
+{
+    public class TaxController : Controller
+    {
+        private IUnitOfWork _unitOfWork;
+
+        public TaxController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var taxYears = (await _unitOfWork.TaxYearRepository.GetAllAsync())
+                .Select(taxYear=>new TaxYearIndexViewModel()
+                {
+                    Id=taxYear.Id,
+                    YearOfTax=taxYear.YearOfTax
+                });
+
+            return View(taxYears);
+        }
+    }
+}
---
+++ PaymentComputeApp.WebUI/Models/TaxYearIndexViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PaymentComputeApp.WebUI.Models
+{
+    public class TaxYearIndexViewModel
+    {
+        public int Id { get; set; }
+        public string YearOfTax { get; set; }
+    }
+}
