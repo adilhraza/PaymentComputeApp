9507e0b3ab7c1aad4e92b0e0929358dc5ba93461
andrija-mitrovic <57568556+andrija-mitrovic@users.noreply.github.com>
2020-07-01 08:25:35 +0200

    StatisticController Added

---
+++ PaymentComputeApp.WebUI/Controllers/StatisticController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PaymentComputeApp.DataAccess.Repositories;
+
+namespace PaymentComputeApp.WebUI.Controllers
+{
+    public class StatisticController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public StatisticController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
