5e977d9e407eb710dcd5d2475139f1ea76dcda2d
andrija-mitrovic <57568556+andrija-mitrovic@users.noreply.github.com>
2020-07-24 19:22:20 +0200

    ExcelExtensions Stylize Excel Document

--- PaymentComputeApp.WebUI/Extensions/ExcelExtensions.cs
+++ PaymentComputeApp.WebUI/Extensions/ExcelExtensions.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using OfficeOpenXml;
+using OfficeOpenXml.FormulaParsing.Excel.Functions.Text;
+using OfficeOpenXml.Style;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -17,6 +19,7 @@ namespace PaymentComputeApp.WebUI.Extensions
 
             CreateExcelHeader(sheet, props, 1);
             CreateExcelDataRows(sheet, data, props, 2);
+            StyleExcelDocument(sheet, props, data, 1);
         }
 
         private static void CreateExcelHeader(ExcelWorksheet sheet, PropertyDescriptorCollection props, int startRow)
@@ -39,6 +42,44 @@ namespace PaymentComputeApp.WebUI.Extensions
                 startRow++;
             }
             sheet.Cells["A:AZ"].AutoFitColumns();
+        }
+
+        private static void StyleExcelDocument<T>(ExcelWorksheet sheet, PropertyDescriptorCollection props, IEnumerable<T> data, int startRow)
+        {
+            StyleExcelHeader(sheet, props, data, startRow);
+
+            startRow += 1;
+            StyleExcelDataRow(sheet, props, data, startRow);
+        }
+
+        private static void StyleExcelHeader<T>(ExcelWorksheet sheet, PropertyDescriptorCollection props, IEnumerable<T> data, int startRow)
+        {
+            for (int i = 1; i <= props.Count; i++)
+            {
+                sheet.Cells[startRow, i].Style.Border.Top.Style = ExcelBorderStyle.Thin;
+                sheet.Cells[startRow, i].Style.Border.Left.Style = ExcelBorderStyle.Thin;
+                sheet.Cells[startRow, i].Style.Border.Bottom.Style = ExcelBorderStyle.Thin;
+                sheet.Cells[startRow, i].Style.Border.Right.Style = ExcelBorderStyle.Thin;
+                sheet.Cells[startRow, i].Style.Font.Bold = true;
+                sheet.Cells[startRow, i].Value = props[i - 1].DisplayName;
+                sheet.Cells[startRow, i].Style.Fill.PatternType = ExcelFillStyle.Solid;
+                sheet.Cells[startRow, i].Style.Fill.BackgroundColor.SetColor(System.Drawing.Color.DodgerBlue);
+            }
+        }
+
+        private static void StyleExcelDataRow<T>(ExcelWorksheet sheet, PropertyDescriptorCollection props, IEnumerable<T> data, int startRow)
+        {
+            foreach (T item in data)
+            {
+                for (int i = 1; i <= props.Count; i++)
+                {
+                    sheet.Cells[startRow, i].Style.Border.Top.Style = ExcelBorderStyle.Thin;
+                    sheet.Cells[startRow, i].Style.Border.Left.Style = ExcelBorderStyle.Thin;
+                    sheet.Cells[startRow, i].Style.Border.Bottom.Style = ExcelBorderStyle.Thin;
+                    sheet.Cells[startRow, i].Style.Border.Right.Style = ExcelBorderStyle.Thin;
+                }
+                startRow++;
+            }
         }
     }
 }
